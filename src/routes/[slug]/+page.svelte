11e7eb90589d2ce285caa5da962f0ca1306beb0c
JoeTheCookie217 <56717228+JoeTheCookie217@users.noreply.github.com>
2023-08-04 15:18:14 +0200

    Initial commit

---
+++ src/routes/[slug]/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { page } from '$app/stores';
+	import { WalletClient } from '@massalabs/massa-web3';
+
+	let privateKey = '';
+	let address = '';
+
+	async function getAddress() {
+		console.log(privateKey);
+		let wallet = await WalletClient.getAccountFromSecretKey(privateKey);
+		if (wallet && wallet.address) address = wallet.address;
+		console.log(address);
+	}
+</script>
+
+<h1>
+	{$page.params.slug}
+</h1>
+<h2>Address: {address}</h2>
