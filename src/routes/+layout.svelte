295e869f6c8d58aa5bad7d0b44d833ce69778d6e
n1pu <nipudos-01@yahoo.com>
2023-09-10 01:00:27 +0200

    chore: update logo & favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,8 +15,35 @@
 
 <main class="h-screen flex flex-col">
 	<header class="flex justify-around items-center p-2">
-		<nav class="flex items-center gap-4">
-			<a class="font-medium" href="/">massa-tools</a>
+		<nav class="flex items-center gap-8">
+			<a class="font-medium text-lg flex items-center gap-2" href="/">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" class="h-6 w-6">
+					<rect width="256" height="256" fill="none" />
+					<line
+						x1="208"
+						y1="128"
+						x2="128"
+						y2="208"
+						fill="none"
+						stroke="currentColor"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="16"
+					/>
+					<line
+						x1="192"
+						y1="40"
+						x2="40"
+						y2="192"
+						fill="none"
+						stroke="currentColor"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="16"
+					/>
+				</svg>
+				<span>massa-tools</span>
+			</a>
 			{#each ['explorer', 'create', 'multisig' /* , 'staking' */] as link}
 				<a
 					href="/{link.toLowerCase()}"
