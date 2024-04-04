f27266774dd15115bceb055d2c88732e4e1a07f3
n1pu <nipudos-01@yahoo.com>
2024-04-05 01:23:05 +0200

    feat: smart decode Dusa events

--- src/routes/event/+page.svelte
+++ src/routes/event/+page.svelte
@@ -3,14 +3,15 @@
 	import Input from '$lib/components/ui/input/input.svelte';
 	import { Label } from '$lib/components/ui/label';
 	import { fetchEvents } from '$lib/services/datastore';
-	import clientStore from '$lib/store/client';
 	import { EventDecoder } from '@dusalabs/sdk';
 	import type { IEvent } from '@massalabs/massa-web3';
-	import { get } from 'svelte/store';
 
 	let txHash = '';
 	let events: IEvent[] = [];
 	let showDetails = false;
+	let decodeSmart = false;
+
+	const extractKeyword = (bytes: string): string => bytes.split(':')[0];
 
 	const handleSearch = async (txId: string) => {
 		console.log('handleSearch', txId);
@@ -27,9 +28,15 @@
 
 <Input bind:value={txHash} />
 
-<div class="flex gap-2">
-	<Label for="details">Show details</Label>
-	<Checkbox id="details" bind:checked={showDetails} />
+<div class="flex flex-col gap-2">
+	<div class="flex gap-2">
+		<Label for="details">Show details</Label>
+		<Checkbox id="details" bind:checked={showDetails} />
+	</div>
+	<div class="flex gap-2">
+		<Label for="smart">Smart decode (Dusa events)</Label>
+		<Checkbox id="smart" bind:checked={decodeSmart} />
+	</div>
 </div>
 {#if events.length}
 	{#each events as event}
@@ -39,7 +46,22 @@
 			</div>
 		{:else}
 			<div>
-				{EventDecoder.decodeError(event.data)}
+				{#if decodeSmart}
+					{@const method = event.data.startsWith('SWAP:')
+						? EventDecoder.decodeSwap
+						: event.data.startsWith('DEPOSITED_TO_BIN:') ||
+						  event.data.startsWith('WITHDRAWN_FROM_BIN:')
+						? EventDecoder.decodeLiquidity
+						: event.data.startsWith('FEES_COLLECTED:')
+						? EventDecoder.decodeLiquidity
+						: event.data.startsWith('TransferSingle:')
+						? EventDecoder.decodeLBTransfer
+						: () => event.data}
+					{EventDecoder.extractParams(event.data).length ? extractKeyword(event.data) : null}
+					{JSON.stringify(method(event.data))}
+				{:else}
+					{EventDecoder.decodeError(event.data)}
+				{/if}
 			</div>
 		{/if}
 	{/each}
