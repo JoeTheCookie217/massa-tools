65eeeaa7036fb14c9a2f50dd41803d618fdca6c8
n1pu <nipudos-01@yahoo.com>
2024-04-26 15:08:55 +0200

    fix(search): redirect to corresponding page

--- src/lib/components/search-bar.svelte
+++ src/lib/components/search-bar.svelte
@@ -1,11 +1,24 @@
 <script lang="ts">
+	import { isAddress, isToken } from '$lib/utils/methods';
 	import { Input } from './ui/input';
+	import { goto } from '$app/navigation';
 
 	let address: string;
 
 	const onKeyDown = (event: KeyboardEvent) => {
 		if (event.key === 'Enter') {
-			console.log('Enter key pressed');
+			const value = (event.target as HTMLInputElement).value;
+			console.log('Enter key pressed', value);
+			if (isToken(value)) {
+				address = '';
+				goto(`/token/${value}`);
+			} else if (isAddress(value)) {
+				address = '';
+				goto(`/explorer/${value}`);
+			} else if (value.startsWith('O1')) {
+				address = '';
+				goto('/event');
+			}
 		}
 	};
 </script>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,5 +3,4 @@
 		Watch everyone, everything, <br /> all at once on
 		<span class="bg-lime-300 px-2 -mx-2">Massa</span>.
 	</h1>
-	<p class="text-lg">This is a page</p>
 </div>
