73fe4c358e2a86b675a247a902cecc1ecebf28ae
n1pu <nipudos-01@yahoo.com>
2024-05-06 20:53:35 +0200

    fix: add chain select btn back

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import { SvelteToast } from '@zerodevx/svelte-toast';
 	import type { SvelteToastOptions } from '@zerodevx/svelte-toast/stores';
 	import ConnectModal from '$lib/components/connect-modal.svelte';
+	import ChainSelect from '$lib/components/chain-select.svelte';
 	import LightSwitch from '$lib/components/light-switch/light-switch.svelte';
 	import { cn } from '$lib/utils';
 	import { page } from '$app/stores';
@@ -63,6 +64,7 @@
 		<SearchBar />
 		<div class="flex items-center gap-2">
 			<ConnectModal />
+			<ChainSelect />
 			<LightSwitch />
 		</div>
 	</header>
