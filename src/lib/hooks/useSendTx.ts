e86291ef8a97893ee3cded9ca2be950ac5f74148
Nils <nipudos-01@yahoo.com>
2024-06-03 16:48:58 +0200

    fix(wallet): display invalid network

--- src/lib/hooks/useSendTx.ts
+++ src/lib/hooks/useSendTx.ts
@@ -4,6 +4,7 @@ import clientStore from '$lib/store/client';
 import { toast } from '@zerodevx/svelte-toast';
 import { EventDecoder } from '@dusalabs/sdk';
 import { fetchEvents } from '$lib/services/datastore';
+import { MASSA_CHAIN_ID } from '$lib/utils/config';
 
 type TState = {
 	txId: string | null;
@@ -32,6 +33,9 @@ const useSendTx = () => {
 		try {
 			if (!massaClient || !massaClient.wallet().getBaseAccount())
 				throw new Error('Massa client is not initialized');
+
+			const walletChainId = (await massaClient.publicApi().getNodeStatus()).chain_id;
+			if (walletChainId !== MASSA_CHAIN_ID) throw new Error('Invalid network');
 
 			const txId = await massaClient.smartContracts().callSmartContract(data);
 			update((state) => ({ ...state, txId }));
@@ -68,6 +72,10 @@ const useSendTx = () => {
 			throw error;
 		}
 	};
+
+	subscribe((state) => {
+		if (state.txId) console.log(state);
+	});
 
 	return {
 		subscribe,
--- src/lib/utils/config.ts
+++ src/lib/utils/config.ts
@@ -1,4 +1,5 @@
 import { ChainId } from '@dusalabs/sdk';
+import { CHAIN_ID as MassaChainId } from '@massalabs/massa-web3';
 
 export const MULTISIG_DEPLOYER: { [chainId in ChainId]: string } = {
 	[ChainId.MAINNET]: '',
@@ -14,6 +15,13 @@ if (!import.meta.env.VITE_CHAIN_ID || !import.meta.env.VITE_CHAIN_URL)
 export const CHAIN_ID = import.meta.env.VITE_CHAIN_ID as any as ChainId;
 export const CHAIN_URL = import.meta.env.VITE_CHAIN_URL as string;
 export const CHAIN_NAME = Number(CHAIN_ID) === Number(ChainId.BUILDNET) ? 'buildnet' : 'mainnet';
+export const MASSA_CHAIN_ID =
+	Number(CHAIN_ID) === Number(ChainId.BUILDNET) ? MassaChainId.BuildNet : MassaChainId.MainNet;
 
 // max number of datastore entries to fetch in a single request
 export const MAX_PER_REQUEST = 1000;
+
+export const FEATURE_FLAGS = {
+	CREATE_TOKEN: false,
+	CREATE_MULTISIG: false
+};
